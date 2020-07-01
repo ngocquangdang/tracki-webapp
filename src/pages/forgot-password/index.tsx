b166c0041b1895e157ba703506085947bfe61cf4
Hanh Tran <hanh.tv204@gmail.com>
2020-07-01 08:56:58 +0700

    fix: warning

--- src/pages/forgot-password/index.tsx
+++ src/pages/forgot-password/index.tsx
@@ -5,7 +5,7 @@ import { withTranslation } from '@Server/i18n';
 import ForgotContainer from '@Containers/auth/ForgotPassword';
 import IForgotPage from '@Containers/auth/ForgotPassword/interfaces';
 
-const Forgot: NextPage = (props: IForgotPage.IProps) => {
+const Forgot: NextPage = (props) => {
   return <ForgotContainer {...props}/>;
 };
 
