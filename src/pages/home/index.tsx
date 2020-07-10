574ea5ee0429dca88ce9a7061e9baa48b2430a00
Sinh Nguyen <nguyenxuansinh0507@gmail.com>
2020-07-10 18:07:23 +0700

    feat: Implement mapbox

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import { MainLayout } from '@Layouts';
+import Map from '@Containers/Map';
 
 const Home = (props: any) => {
-  return <MainLayout {...props}>Home content</MainLayout>;
+  return (
+    <MainLayout {...props}>
+      <Map />
+    </MainLayout>
+  );
 };
 
 export default Home;
