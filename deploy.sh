a31b6fee5215a31a39074ee83299df642f53474e
Tân Nguyễn <tannguyenit95@gmail.com>
2020-07-15 02:27:13 +0000

    deploy.sh edited online with Bitbucket

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,13 @@
 
 echo "##### Login docker private registry....."
 echo "$1" | docker login -u "$2" --password-stdin
-echo "##### Stop all running containers....."
+echo "##### Stop all running containers and remove old image ....."
 docker stop tracki-frontend 
-echo "##### Stop all running containers....."
+docker rmi $(docker images |grep 'mtrk2021/tracki-frontend')
+echo "##### Stop all running containers ....."
 docker rm tracki-frontend 
-echo "##### Pull image from docker registry....."
+echo "##### Pull image from docker registry ....."
 docker pull $3
-echo "##### Run image....."
+echo "##### Run image ....."
 docker run -d --name tracki-frontend -p 3001:3000 --restart unless-stopped $3
 echo "##### Successfully"
