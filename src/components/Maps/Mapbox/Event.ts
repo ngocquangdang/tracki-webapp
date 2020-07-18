a25e3182c2dc7549f2f20c2307952d2d0700fe6a
Hanh Tran <hanh.tv204@gmail.com>
2020-07-18 11:58:06 +0700

    fix: build

--- src/components/Maps/Mapbox/Event.ts
+++ src/components/Maps/Mapbox/Event.ts
@@ -1,6 +1,12 @@
 import mapboxgl from 'mapbox-gl';
 import geoViewport from '@mapbox/geo-viewport';
 
+interface COORDINATE {
+  lat: number;
+  lng: number;
+  [data: string]: any;
+}
+
 class MapBoxEvent {
   mapApi: any;
   constructor(mapRef) {
@@ -27,11 +33,12 @@ class MapBoxEvent {
     this.mapApi.zoomTo(nextZoom);
   };
 
-  setFitBounds = (coordinates: Array<any>, callback) => {
+  setFitBounds = (coordinates: COORDINATE[], callback) => {
     const newCoordinates = coordinates.map(({ lat, lng }) => [lng, lat]);
+    const [lngFirst, latFirst] = newCoordinates[0];
     const bounds = newCoordinates.reduce(
-      (result, coord) => result.extend(coord),
-      new mapboxgl.LngLatBounds(newCoordinates[0], newCoordinates[0])
+      (result, [lng, lat]) => result.extend([lng, lat]),
+      new mapboxgl.LngLatBounds([lngFirst, latFirst], [lngFirst, latFirst])
     );
     const arrayBound = bounds.toArray();
     const zoomWithGeo = coordinates.length === 1 ? 18 : 20;
