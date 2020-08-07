2b2c88fcd0b8fe50371f2e99e0827640dadaa0f8
Hanh Tran <hanh.tv204@gmail.com>
2020-08-07 19:44:25 +0700

    fix: create circle geo

--- src/components/GeofenceListPC/components/GeoFenceCard/styles.ts
+++ src/components/GeofenceListPC/components/GeoFenceCard/styles.ts
@@ -16,6 +16,7 @@ const useStyles = makeStyles(() => ({
     height: 56,
     width: '100%',
     borderRadius: 3,
+    border: 'solid 1px rgba(0,0,0,.04)',
     boxShadow:
       '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)',
   },
