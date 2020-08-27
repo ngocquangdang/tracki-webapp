68d3f26b79e4149096479ed96d8f575ec5473861
Quang Dang <ngocquang.nq98@gmail.com>
2020-08-27 14:00:38 +0700

    remove .env

--- src/pages/trackers/[id]/renew/index.tsx
+++ src/pages/trackers/[id]/renew/index.tsx
@@ -6,15 +6,15 @@ import { IPage } from '@Interfaces';
 import withAuth from '@Components/hocs/withAuth';
 import View from '@Containers/Renew';
 
-const TrackersView: NextPage<IPage.InitialProps> = props => {
+const TrackerRenew: NextPage<IPage.InitialProps> = props => {
   return <View {...props} />;
 };
 
-TrackersView.getInitialProps = async (): Promise<IPage.InitialProps> => {
+TrackerRenew.getInitialProps = async (): Promise<IPage.InitialProps> => {
   return { namespacesRequired: ['common', 'tracker', 'subscription'] };
 };
 
 export default compose(
   withAuth,
   withTranslation(['common', 'tracker', 'subscription'])
-)(TrackersView);
+)(TrackerRenew);
