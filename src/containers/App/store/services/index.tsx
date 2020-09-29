83d4fc47320f4aee1dcac62e04a1787a77d5940b
Sinh Nguyen <nguyenxuansinh0507@gmail.com>
2020-09-29 15:51:42 +0700

    feat: Implement logout geobot

--- src/containers/App/store/services/index.tsx
+++ src/containers/App/store/services/index.tsx
@@ -1,4 +1,5 @@
 import axiosClient from '@Utils/axios';
+import axiosClientGeobot from '@Utils/axiosGeobot';
 
 const USER_ENDPOINT = '/v3/user';
 const LOGOUT_ENDPOINT = '/internal/v1/user/logout';
@@ -9,4 +10,22 @@ export const fetchUser = async () => {
 
 export const logout = async () => {
   return await axiosClient.post(LOGOUT_ENDPOINT);
+};
+
+export const logoutGeobotTracki = async (
+  token: string,
+  refreshToken: string,
+  expiresIn: number
+) => {
+  await axiosClientGeobot.post(
+    `/setToken`,
+    {
+      token,
+      refreshToken,
+      expiresIn,
+    },
+    {
+      withCredentials: true,
+    }
+  );
 };
