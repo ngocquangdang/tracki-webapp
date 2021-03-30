266efe27939148acf4347194f9a4b35596e74c53
Quang Dang <ngocquang.nq98@gmail.com>
2021-03-30 18:51:08 +0700

    feat: TK-227 Social network login

--- src/containers/auth/Login/views/index.tsx
+++ src/containers/auth/Login/views/index.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+import { useSession } from 'next-auth/client';
 
 import { AuthLayout } from '@Layouts';
 import { Button } from '@Components/buttons';
-
 import {
   Container,
   Footer,
@@ -22,13 +22,34 @@ import LoginForm from './form';
 import ChatUs from '../components/ChatUS';
 import Modal from '@Components/modals';
 import { firebaseLogEventRequest } from '@Utils/firebase';
+import axios from '@Utils/axios';
 
 export default function Login(props: ILoginPage.IProps) {
   const { t, resetErrorAction } = props;
   const classes = useStyles();
   const [open, setOpen] = useState(false);
+  const [session] = useSession();
 
   firebaseLogEventRequest('login_page', '');
+
+  useEffect(() => {
+    if (session && session.accessToken) {
+      console.log(
+        '🚀 ~ file: index.tsx ~ line 37 ~ useEffect ~ session.accessToken',
+        session.accessToken.accessToken
+      );
+      const configData = {
+        accessToken: session.accessToken.accessToken,
+        whiteLabel: 'TRACKIMO',
+        client_id: process.env.NEXT_PUBLIC_OAUTH_CLIENT_ID,
+        redirect_uri: process.env.NEXT_PUBLIC_OAUTH_REDIRECT_URI,
+      };
+      axios.post(
+        process.env.NEXT_PUBLIC_API_URL + '/v1/social/login/google',
+        configData
+      );
+    }
+  }, [session]);
 
   const handleOpenModal = () => {
     setOpen(true);
@@ -61,6 +82,20 @@ export default function Login(props: ILoginPage.IProps) {
               onClick={onCreateNewAccount}
             />
           </Link>
+          {!session && (
+            <>
+              Not signed in <br />
+              <a href="/api/auth/signin">sss</a>
+              {/* <button onClick={() => signIn()}>Sign in</button> */}
+            </>
+          )}
+          {session && (
+            <>
+              Signed in as {session.user.email} <br />
+              <a href="/api/auth/signout">sss</a>
+            </>
+          )}
+          <iframe src="/api/auth/session" />
         </Content>
         <Footer>
           <Contact>
