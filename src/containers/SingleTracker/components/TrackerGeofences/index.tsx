d70c40b69067a5667dce5cd246c9ce62047fc06f
Quang Dang <ngocquang.nq98@gmail.com>
2021-04-09 18:54:12 +0700

    fix: fix bug add contact geofence

--- src/containers/SingleTracker/components/TrackerGeofences/index.tsx
+++ src/containers/SingleTracker/components/TrackerGeofences/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { uniqueId } from 'lodash';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
