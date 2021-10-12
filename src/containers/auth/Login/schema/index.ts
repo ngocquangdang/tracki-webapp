b458062f0e623f8d1ec46b86fb05c211d115fa11
HoangThaiNguyenDev <hoang.nguyenthai@digitalfortress.dev>
2021-10-12 10:04:36 +0700

    fix: login ID_001

--- src/containers/auth/Login/schema/index.ts
+++ src/containers/auth/Login/schema/index.ts
@@ -1,10 +1,14 @@
 import * as Yup from 'yup';
 
 export const LoginSchema = Yup.object().shape({
-  username: Yup.string().email('email_invalid').required('required'),
+  username: Yup.string()
+    .min(1, 'too_short')
+    .max(40, 'too_long')
+    .email('email_invalid')
+    .required('required'),
   password: Yup.string()
     .min(1, 'too_short')
-    .max(50, 'too_long')
+    .max(25, 'too_long')
     .required('required'),
 });
 export const ChatUsSchema = Yup.object().shape({
