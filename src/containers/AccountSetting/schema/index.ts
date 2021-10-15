e3e7fc909226961bc3f7ea36ca33ac1d0bf3371a
HoangThaiNguyenDev <hoang.nguyenthai@digitalfortress.dev>
2021-10-15 10:01:26 +0700

    fix: account setting

--- src/containers/AccountSetting/schema/index.ts
+++ src/containers/AccountSetting/schema/index.ts
@@ -3,9 +3,11 @@ import * as Yup from 'yup';
 export const UserSchema = Yup.object().shape({
   first_name: Yup.string()
     .matches(/^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/, 'Invalid first name')
+    .max(40, 'too_long')
     .required('required'),
   last_name: Yup.string()
     .matches(/^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/, 'Invalid last name')
+    .max(40, 'too_long')
     .required('required'),
   phone: Yup.string()
     .matches(/^[0-9]{1,2}[0-9]*$/, 'auth:wrong_phone_format')
