43e4c893c4ce8e7644c4187ff269f15116e78591
HoangThaiNguyenDev <hoang.nguyenthai@digitalfortress.dev>
2021-10-18 10:58:39 +0700

    refactor: format code

--- src/containers/TrackingModes/index.tsx
+++ src/containers/TrackingModes/index.tsx
@@ -31,8 +31,6 @@ export default function TrackingModes(props: Props) {
     handleCloseModal,
     // t,
     trackerSettings,
-    //tracker,
-    //extendsBatteryModeRequest,
     showSnackbar,
     // isRequesting,
     trackingModeRequest,
