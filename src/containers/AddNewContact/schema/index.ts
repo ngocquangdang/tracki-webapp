c46299f908d347e5e542048533ba93775728756e
HoangThaiNguyenDev <hoang.nguyenthai@digitalfortress.dev>
2021-10-30 01:21:42 +0700

    fix: id 141

--- src/containers/AddNewContact/schema/index.ts
+++ src/containers/AddNewContact/schema/index.ts
@@ -10,12 +10,16 @@ export const emailSchema = Yup.object().shape({
 });
 
 export const smsSchema = Yup.object().shape({
-  name: Yup.string().trim('auth:required').required('auth:required'),
+  name: Yup.string()
+    .trim('auth:required')
+    .max(25, 'too_long')
+    .required('auth:required'),
   phone: Yup.string()
     .trim('auth:required')
     .matches(/^[0-9]{1,2}[0-9]*$/, {
       message: 'auth:wrong_phone_format',
       excludeEmptyString: true,
     })
+    .max(45, 'too_long')
     .required('auth:required'),
 });
