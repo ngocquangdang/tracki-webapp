7c5560bd9b5ca14a96b23c5765c34f8ff1ef271b
Quang Dang <ngocquang.nq98@gmail.com>
2022-03-23 10:30:43 +0700

    fix build -2

--- src/pages/trackers/index.tsx
+++ src/pages/trackers/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NextPage } from 'next';
 import { compose } from 'redux';
 import { withTranslation } from 'next-i18next';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+// import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import { IPage } from '@Interfaces';
 import withAuth from '@Components/hocs/withAuth';
@@ -12,13 +12,17 @@ const TrackersView: NextPage<IPage.InitialProps> = props => {
   return <View {...props} />;
 };
 
-export async function getServerSideProps({ locale }) {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, ['common'])),
-      // Will be passed to the page component as props
-    },
-  };
-}
+TrackersView.getInitialProps = async (): Promise<IPage.InitialProps> => {
+  return { namespacesRequired: ['common'] };
+};
+
+// export async function getServerSideProps({ locale }) {
+//   return {
+//     props: {
+//       ...(await serverSideTranslations(locale, ['common'])),
+//       // Will be passed to the page component as props
+//     },
+//   };
+// }
 
 export default compose(withAuth, withTranslation('common'))(TrackersView);
