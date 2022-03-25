9c2ab20970bd7efacd58dcd641997a6e27e26a54
Quang Dang <ngocquang.nq98@gmail.com>
2022-03-25 13:55:44 +0700

    fix build -8

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,12 +1,8 @@
-const { initReactI18next } = require('react-i18next');
-
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de', 'ar', 'ja', 'es', 'pt', 'tr'],
     localeDetection: false,
-    use: [initReactI18next],
     serializeConfig: false,
   },
-  react: { useSuspense: false }, //this li
 };
