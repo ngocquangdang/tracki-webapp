1cf4efba8dd58a0ee44839f0baa633b164cbdd5b
Quang Dang <ngocquang.nq98@gmail.com>
2022-03-25 14:16:47 +0700

    fix build -9

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,13 @@
+// const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de', 'ar', 'ja', 'es', 'pt', 'tr'],
     localeDetection: false,
     serializeConfig: false,
+    // localePath: path.resolve('./public/locales'),
+    localePath: './public/locales',
   },
+  localePath: './public/locales',
 };
