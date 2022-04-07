689ec533beafefeaa4ac2418a560e43820f0feab
Quang Dang <ngocquang.nq98@gmail.com>
2022-04-07 09:51:07 +0700

    fix account setting translation

--- src/containers/AccountSetting/view/ViewContent.tsx
+++ src/containers/AccountSetting/view/ViewContent.tsx
@@ -30,7 +30,6 @@ import UserDatails from '../interfaces';
 import { LANGUAGES, DATE_SETTINGS } from '../store/definitions';
 import { firebaseLogEventRequest } from '@Utils/firebase';
 import { useRouter } from 'next/router';
-import cookie from '@Utils/cookie';
 
 interface SettingState {
   language: {
@@ -128,7 +127,7 @@ export default function AccountSetting(props: any) {
       updateInfoUser(dataUpdateUser);
       updatePrefrence(dataUpdatePreferences);
     }
-    cookie.setCookie('NEXT_LOCALE', value.language);
+    document.cookie = `NEXT_LOCALE=${value.language};path=/`;
     router.replace('/settings', '/settings', { locale: value.language });
   };
 
