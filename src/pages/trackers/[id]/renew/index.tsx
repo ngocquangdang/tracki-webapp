647babd4366dc05e6218c8ad8380076c66cef3c5
Quang Dang <ngocquang.nq98@gmail.com>
2022-04-07 23:00:34 +0700

    Fix renew page not found - 9

--- src/pages/trackers/[id]/renew/index.tsx
+++ src/pages/trackers/[id]/renew/index.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { NextPage } from 'next';
-// import { compose } from 'redux';
-// import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-// import { withTranslation } from 'next-i18next';
+import { compose } from 'redux';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { withTranslation } from 'next-i18next';
 
 import { IPage } from '@Interfaces';
-// import withAuth from '@Components/hocs/withAuth';
+import withAuth from '@Components/hocs/withAuth';
 import View from '@Containers/Renew';
-// import nextI18nextConfig from 'next-i18next.config';
+import nextI18nextConfig from 'next-i18next.config';
 
-// const ns = ['common', 'tracker', 'subscription'];
+const ns = ['common', 'tracker', 'subscription'];
 
 const TrackerRenew: NextPage<IPage.InitialProps> = props => {
   return <View {...props} />;
 };
 
-// export const getStaticProps = async ({ locale }: any) => {
-//   return {
-//     props: {
-//       ...(await serverSideTranslations(locale, ns, nextI18nextConfig)),
-//     },
-//   };
-// };
-// export default compose(withAuth, withTranslation(ns))(TrackerRenew);
-export default TrackerRenew;
+export const getStaticProps = async ({ locale }: any) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ns, nextI18nextConfig)),
+    },
+  };
+};
+export default compose(withAuth, withTranslation(ns))(TrackerRenew);
+// export default TrackerRenew;
